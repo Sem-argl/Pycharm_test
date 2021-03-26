9fa03f9b4cd03298b20abea9b3e6c87b4b947dbb
Sem-argl <31043801+Sem-argl@users.noreply.github.com>
2021-03-26 19:19:40 +0200

    Add files via upload
    beautifull soup working program with comments

---
+++ main.py
@@ -0,0 +1,19 @@
+# import codecademylib3_seaborn
+from bs4 import BeautifulSoup       # імпортуэмо потрібні бібліотеки
+import requests                     # імпортуэмо потрібні бібліотеки
+# підготовчы роботи
+webpage_response = requests.get('https://content.codecademy.com/courses/beautifulsoup/cacao/index.html')
+webpage = webpage_response.content
+# загружаємо у вигляді HTML коду всю сторінку сайту
+soup = BeautifulSoup(webpage, "html.parser")
+# закінчили підготовчы роботи
+table_str = ["Company", "Origin", "REF", "ReviewDate", "CocoaPercent", "CompanyLocation", "Rating", "BeanType",
+             "BroadBeanOrigin"]
+table = [[], [], [], [], [], [], [], [], []]
+for i, clas in enumerate(table_str):
+    
+    for line in soup.select(".{}".format(clas)):
+        # доюавляємо до таблиці значення тексту закріпленого за class знайденого в попередньому рядку
+            table[i].append(line.text)
+
+print("tabl", table)
